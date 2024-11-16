c0a205d9942559607ef03743d72a55ec17abcbc9
Jxkeorton <jakeorton99@gmail.com>
2024-11-16 10:13:49 +0000

    Add navbar icons depending on user context, Fix file structure

--- src/components/ui/navbar/Navbar.js
+++ src/components/ui/navbar/Navbar.js
@@ -5,14 +5,41 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import logo from "../../../assets/logo.png";
-import { useCurrentUser } from "../../../contexts/CurrentUserContext";
+import Avatar from "../avatar/Avatar";
+import { useCurrentUser, useSetCurrentUser } from "../../../contexts/CurrentUserContext";
+import axios from 'axios';
 
 const isActive = (navData) =>
   navData.isActive ? `${styles.NavLink} ${styles.Active}` : styles.NavLink;
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
-  const loggedInIcons = <>{currentUser?.username}</>
+  const setCurrentUser = useSetCurrentUser();
+
+  const handleSignOut = async () => {
+    try {
+      await axios.post("dj-rest-auth/logout/");
+      setCurrentUser(null);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addReviewIcon = (
+    <NavLink className={isActive} to="/reviews/create">
+      <i className="fas fa-plus-square"></i> Add Review
+    </NavLink>
+  );
+  const loggedInIcons = (
+    <>
+      <NavLink className={styles.NavLink} to="/" onClick={handleSignOut}>
+        <i className="fas fa-sign-out-alt"></i> Sign Out
+      </NavLink>
+      <NavLink className={isActive} to={`/profile/${currentUser?.profile_id}`}>
+        <Avatar src={currentUser?.profile_image} text="Profile" height={40} />
+      </NavLink>
+    </>
+  );
   const loggedOutIcons = (
     <>
       <NavLink className={isActive} to="/sign-in">
@@ -32,11 +59,15 @@ const NavBar = () => {
             <img src={logo} alt="logo" height="45" />
           </Navbar.Brand>
         </NavLink>
+        {currentUser && addReviewIcon}
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
           <Nav className="ml-auto">
             <NavLink className={isActive} to="/">
               <i className="fa-solid fa-location-dot"></i> Locations
+            </NavLink>
+            <NavLink className={isActive} to="/reviews">
+              <i className="fa-solid fa-location-dot"></i> Reviews
             </NavLink>
             {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
