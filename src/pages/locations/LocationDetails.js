0b68b01a986dc7ce6c24a63dba240e599d1ff81f
Jxkeorton <jakeorton99@gmail.com>
2024-12-08 14:36:05 +0000

    Add isloading spinner to location details page

--- src/pages/locations/LocationDetails.js
+++ src/pages/locations/LocationDetails.js
@@ -1,37 +1,56 @@
-import React, {useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import DetailsCard from "../../components/locations/DetailsCard";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefault";
-import ReviewsList from "../../components/reviews/ReviewsList"
+import ReviewsList from "../../components/reviews/ReviewsList";
+import { Spinner, Container } from "react-bootstrap";
 
 const LocationDetails = () => {
   const { id } = useParams();
   const [location, setLocation] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const handleMount = async () => {
       try {
+        setIsLoading(true);
         const [{ data: location }] = await Promise.all([
           axiosReq.get(`/locations/${id}/`),
         ]);
         setLocation(location);
       } catch (err) {
         console.log(err);
+        setIsLoading(false);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     handleMount();
   }, [id]);
 
-  if (!location) {
+  if (!location && !isLoading) {
     return <p>Location not found!</p>;
   }
 
   return (
-    <div>
-      <DetailsCard location={location} />
-      <ReviewsList location={location} />
-    </div>
+    <>
+      {isLoading ? (
+        <Container
+          className="d-flex justify-content-center align-items-center"
+          style={{ minHeight: "100px" }}
+        >
+          <Spinner animation="border" role="status" variant="primary">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </Container>
+      ) : (
+        <div>
+          <DetailsCard location={location} />
+          <ReviewsList location={location} />
+        </div>
+      )}
+    </>
   );
 };
 
