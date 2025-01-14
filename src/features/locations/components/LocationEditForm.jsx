46ce4458b2c8af1c1cf036a2192afa09216ae49c
Jxkeorton <jakeorton99@gmail.com>
2025-01-14 20:11:45 +0000

    Add admin components

---
+++ src/features/locations/components/LocationEditForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LocationEditForm = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default LocationEditForm
---
+++ src/features/profile/components/ContactMessageList.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ContactMessageList = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ContactMessageList
